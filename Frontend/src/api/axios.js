74739fab803fca9b6f4c3b24094eed78c3bedb6e
iamarpitsharma <90828065+iamarpitsharma@users.noreply.github.com>
2025-07-03 11:20:11 +0530

    commit

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -25,9 +25,19 @@
 
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "https://cryptalk-backend.onrender.com/api",
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+    // || "http://localhost:5000/api",
   withCredentials: true,
 });
 
-export default instance;
+// Optional: add interceptor to attach token automatically
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("cryptalk_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default axiosInstance;
